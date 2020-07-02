f916f5c63168309e34cad9703b8db93cca1049b5
MarceSanto <rocha.mer21@gmail.com>
2020-07-02 11:02:49 -0300

    função pura e impura

---
+++ funcional/funcao_pura_3.js
@@ -0,0 +1,17 @@
+
+let qtdeDeExecucoes = 0
+
+// Pura!
+function somar(a, b) {
+  qtdeDeExecucoes++
+  return a + b
+}
+
+console.log(qtdeDeExecucoes)
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(somar(68, 31))
+console.log(qtdeDeExecucoes)
