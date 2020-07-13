8e50d738095e0349986db4a6f9e05f5b6290ea60
MarceSanto <rocha.mer21@gmail.com>
2020-07-13 09:17:36 -0300

    padrao observer

---
+++ observer/festa_surpresa.js
@@ -0,0 +1,49 @@
+const readline = require('readline')
+
+function obterResposta(pergunta) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  return new Promise((resolve) => {
+    rl.question(pergunta, (resp) => {
+      resolve(resp)
+      rl.close()
+    })
+  })
+}
+
+// Observer
+function namorada() {
+  console.log('N: Apagar as luzes...')
+  console.log('N: Pedir silêncio...')
+  console.log('N: Surpresa!!!!')
+}
+
+// Observer
+function sindico() {
+  console.log('S: Monitorando o barulho!')
+}
+
+// Subject
+async function porteiro(interessados) {
+  while (true) {
+    const resp = await obterResposta('O namorado chegou? (s/N/q) ')
+    if (resp.toLowerCase() === 's') {
+      // os observadores sao notificados
+      ;(interessados || []).forEach((obs) => obs())
+      break
+    } else if (resp.toLowerCase() === 'q') {
+      break
+    }
+  }
+}
+
+/*
+  Chamada da Função -> Registra dois observadores
+  Os Observadores são: namorada e sindico
+  O subject é o porteiro!
+*/
+
+porteiro([namorada, sindico]) 
